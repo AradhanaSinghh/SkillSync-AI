b3206e7a6b0ed39794c5a0d2218a5a0bc356c063
Your name <aradhanaswan3456@gmail.com>
2026-08-16 00:33:59 +0530

    updated

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,12 @@
 const mongoose=require('mongoose');
 
-async function connectT
+async function connectToDB(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("✅Mongodb connnected successfully!");
+    }
+    catch(error){
+        console.log("❌Mongodb connection failed!",error);
+    }
+}
+module.exports=connectToDB
