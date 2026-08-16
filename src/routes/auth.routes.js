d50426eb2d36f5455cba9de3bf4ebea857335c97
Your name <aradhanaswan3456@gmail.com>
2026-08-16 19:46:19 +0530

    commit

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,6 @@
+const {Router} =require('express');
+const authController=require("../controllers/auth.controller.js")
+const authRouter=Router();
+
+authRouter.post("/register",authController.registerUserController)
+module.exports=authRouter;
